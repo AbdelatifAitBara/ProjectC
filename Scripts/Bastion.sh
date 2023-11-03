06a5b01a8bfeea0eff21cc23fa7153b387ab1cbd
Abdelatif AIT BARA <aitbaraabdelatif@outlook.com>
2023-11-03 02:57:23 +0100

    Added Bastion.sh

---
+++ Scripts/Bastion.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+sudo apt update
+echo "deb [signed-by=/usr/share/keyrings/hashicorp.gpg] https://apt.releases.hashicorp.com $(lsb_release -cs) main" | sudo tee /etc/apt/sources.list.d/hashicorp.list
+sudo apt update
+sudo apt install terraform
