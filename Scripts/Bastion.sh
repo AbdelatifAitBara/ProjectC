633644c2ccf881f82900a97766978da3fedbcd57
Abdelatif AIT BARA <aitbaraabdelatif@outlook.com>
2023-11-03 09:57:30 +0100

    Edited Scripts.sh

--- Scripts/Bastion.sh
+++ Scripts/Bastion.sh
@@ -4,3 +4,4 @@ curl -fsSL https://apt.releases.hashicorp.com/gpg | sudo gpg --dearmor -o /usr/s
 echo "deb [signed-by=/usr/share/keyrings/hashicorp.gpg] https://apt.releases.hashicorp.com $(lsb_release -cs) main" | sudo tee /etc/apt/sources.list.d/hashicorp.list
 sudo apt update
 sudo apt install terraform
+sudo apt install -y awscli
