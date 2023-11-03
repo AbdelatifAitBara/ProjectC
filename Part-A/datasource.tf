3a88865f318e8cf223521bd85a5a2677920af089
Abdelatif AIT BARA <aitbaraabdelatif@outlook.com>
2023-11-03 03:50:55 +0100

    Edited ec2.tf

--- Part-A/datasource.tf
+++ Part-A/datasource.tf
@@ -1,14 +1,15 @@
-/*
-
 data "aws_instance" "ec2_vault" {
   instance_id = aws_instance.ec2_vault.id
 }
 
 data "template_file" "install_jenkins" {
   template = file("./install_jenkins.tftpl")
-  vars = {
-    ec2_vault_ip = aws_instance.ec2_vault.public_ip
-  }
 }
 
-*/
+data "template_file" "install_ansible" {
+  template = file("./install_ansible.tftpl")
+}
+
+data "template_file" "install_vault" {
+  template = file("./install_vault.tftpl")
+}
--- Part-A/ec2.tf
+++ Part-A/ec2.tf
@@ -6,6 +6,7 @@ resource "aws_instance" "ec2_jenkins" {
   vpc_security_group_ids = [aws_security_group.sg_jenkins.id]
   subnet_id              = var.public_subnet_1
   key_name               = var.ec2_key_name
+  iam_instance_profile   = var.terraform_role
   user_data              = data.template_file.install_jenkins.rendered
 
   tags = {
@@ -32,6 +33,8 @@ resource "aws_instance" "ec2_ansible" {
   vpc_security_group_ids = [aws_security_group.sg_ansible.id]
   subnet_id              = var.public_subnet_1
   key_name               = var.ec2_key_name
+  iam_instance_profile   = var.terraform_role
+  user_data              = data.template_file.install_ansible.rendered
 
   tags = {
     Name     = "Abdelatif-EC2-02"
@@ -57,6 +60,8 @@ resource "aws_instance" "ec2_vault" {
   vpc_security_group_ids = [aws_security_group.sg_vault.id]
   subnet_id              = var.private_subnet_1
   key_name               = var.ec2_key_name
+  iam_instance_profile   = var.terraform_role
+  user_data              = data.template_file.install_vault.rendered
 
 
   root_block_device {
