2ab8f753afd193e722a00ba89f9c91beccc3eff7
Abdelatif AIT BARA <aitbaraabdelatif@outlook.com>
2023-11-03 03:16:12 +0100

    Edited Outputs.tf

--- Part-A/outputs.tf
+++ Part-A/outputs.tf
@@ -1,3 +1,16 @@
+# Jenkins EC2 OUTPUTS
+
+output "ec2_jenkins_ip" {
+  value = aws_instance.ec2_jenkins.public_ip
+}
+
+# Ansible EC2 OUTPUTS
+
+output "ec2_ansible_ip" {
+  value = aws_instance.ec2_ansible.public_ip
+}
+
+
 # Vault EC2 OUTPUTS
 
 output "ec2_vault_ip" {
