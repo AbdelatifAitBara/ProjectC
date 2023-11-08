86bd342b2db8877cba341f5d956028266eafd24b
Abdelatif AIT BARA <aitbaraabdelatif@outlook.com>
2023-11-08 10:32:52 +0100

    Some fix

--- Part-B/sg.tf
+++ Part-B/sg.tf
@@ -42,12 +42,13 @@ resource "aws_security_group" "bm-sg" {
     cidr_blocks = ["0.0.0.0/0"]
   }
 
-  egress = {
+  egress {
     from_port   = 0
     to_port     = 0
     protocol    = "-1"
     cidr_blocks = ["0.0.0.0/0"]
   }
+  
 }
 
 
