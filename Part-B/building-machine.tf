6e9160fbc31aa524bdf8f886b5c0a908d1b5c58e
Abdelatif AIT BARA <aitbaraabdelatif@outlook.com>
2023-11-08 10:57:21 +0100

    Some fix

--- Part-B/building-machine.tf
+++ Part-B/building-machine.tf
@@ -1,6 +1,7 @@
 # Deploy auto scaling group For My Building Machine
 
 resource "aws_launch_configuration" "alc" {
+  name            = "Abdelatif-BM"
   image_id        = var.ami
   instance_type   = var.instance_type
   security_groups = [aws_security_group.bm-sg.id]
@@ -9,12 +10,13 @@ resource "aws_launch_configuration" "alc" {
 }
 
 
-resource "aws_autoscaling_group" "asg" {
+resource "aws_autoscaling_group" "abdelatif-bm-asg" {
+  name                  = "Abdelatif-BM-ASG"
   launch_configuration = aws_launch_configuration.alc.name
   vpc_zone_identifier  = [aws_subnet.PublicSubnet01.id, aws_subnet.PublicSubnet02.id]
 
   target_group_arns = [aws_lb_target_group.abdelatif-tg.arn]
-  health_check_type = "ELB"
+  health_check_type = "EC2"
 
   min_size         = 2
   max_size         = 4
