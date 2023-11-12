27e332316e77f1f9ff11c4a2672ed8403cc70070
Abdelatif AIT BARA <aitbaraabdelatif@outlook.com>
2023-11-12 13:41:04 +0100

    Test new JF V9

--- Microservices/Product/product.py
+++ Microservices/Product/product.py
@@ -12,7 +12,7 @@ import re
 app = Flask(__name__)
 CORS(app)
 
-app.config['MYSQL_DATABASE_USER'] = "root"
+app.config['MYSQL_DATABASE_USER'] = os.getenv('MYSQL_DATABASE_USER')
 app.config['MYSQL_DATABASE_PASSWORD'] = os.getenv('MYSQL_DATABASE_PASSWORD')
 app.config['MYSQL_DATABASE_DB'] = os.getenv('MYSQL_DATABASE_DB')
 app.config['MYSQL_DATABASE_HOST'] = os.getenv('MYSQL_DATABASE_HOST')
