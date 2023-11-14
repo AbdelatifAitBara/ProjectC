fa7d3a538658048c22a7f24c971d1ae992474da4
Abdelatif AIT BARA <aitbaraabdelatif@outlook.com>
2023-11-14 16:32:12 +0100

    Alpha 5 version of microservices

--- Part-B/nodes.tf
+++ Part-B/nodes.tf
@@ -14,7 +14,7 @@ resource "aws_eks_node_group" "private-nodes" {
   instance_types = ["t3.small"]
 
   scaling_config {
-    desired_size = 2
+    desired_size = 3
     max_size     = 6
     min_size     = 2
   }
