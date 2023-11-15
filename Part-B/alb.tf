1e00679e28ba89a9b9bdd47f9d011225ac17a34f
Abdelatif AIT BARA <aitbaraabdelatif@outlook.com>
2023-11-15 12:30:41 +0100

    Some modifications

--- Part-B/alb.tf
+++ Part-B/alb.tf
@@ -39,33 +39,6 @@ resource "aws_lb_target_group" "abdelatif-tg-HTTP" {
   }
 }
 
-# Add the same TG but on HTTPS:
-
-resource "aws_lb_target_group" "abdelatif-tg-HTTPS" {
-  name        = "Abdelatif-TG-HTTPS"
-  target_type = "instance"
-  port        = 32001
-  protocol    = "HTTPS"
-  vpc_id      = var.vpc_id
-
-  health_check {
-    path                = "/healthz"
-    protocol            = "HTTPS"
-    matcher             = "200"
-    interval            = 15
-    timeout             = 5
-    healthy_threshold   = 2
-    unhealthy_threshold = 3
-  }
-
-  tags = {
-    Name     = "Abdelatif-TG-HTTPS"
-    owner    = local.tags.owner
-    ephemere = local.tags.ephemere
-    entity   = local.tags.entity
-  }
-}
-
 resource "aws_autoscaling_attachment" "asg_attachment1" {
   autoscaling_group_name = aws_eks_node_group.private-nodes.resources[0].autoscaling_groups[0].name
   lb_target_group_arn    = aws_lb_target_group.abdelatif-tg-HTTP.arn
@@ -75,14 +48,6 @@ resource "aws_autoscaling_attachment" "asg_attachment1" {
   ]
 }
 
-resource "aws_autoscaling_attachment" "asg_attachment2" {
-  autoscaling_group_name = aws_eks_node_group.private-nodes.resources[0].autoscaling_groups[0].name
-  lb_target_group_arn    = aws_lb_target_group.abdelatif-tg-HTTPS.arn
-
-  depends_on = [
-    aws_eks_node_group.private-nodes
-  ]
-}
 
 resource "aws_lb_listener" "abdelatif-lb-listener-http" {
   load_balancer_arn = aws_lb.abdelatif-alb.arn
@@ -102,20 +67,3 @@ resource "aws_lb_listener" "abdelatif-lb-listener-http" {
   }
 }
 
-resource "aws_lb_listener" "abdelatif-lb-listener-https" {
-  load_balancer_arn = aws_lb.abdelatif-alb.arn
-  port              = "443"
-  protocol          = "HTTPS"
-
-  default_action {
-    type             = "forward"
-    target_group_arn = aws_lb_target_group.abdelatif-tg-HTTPS.arn
-  }
-
-  tags = {
-    Name     = "abdelatif-lb-listener-https"
-    owner    = local.tags.owner
-    ephemere = local.tags.ephemere
-    entity   = local.tags.entity
-  }
-}
