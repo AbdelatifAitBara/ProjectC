3279b6015fe1b9e70b2a8db87bb9832e47b4a6c1
Abdelatif AIT BARA <aitbaraabdelatif@outlook.com>
2023-11-16 11:09:13 +0100

    Added HTTPS TG with Certificate

--- Part-B/alb.tf
+++ Part-B/alb.tf
@@ -39,9 +39,45 @@ resource "aws_lb_target_group" "abdelatif-tg-HTTP" {
   }
 }
 
+# Create TG with HTTPS
+
+resource "aws_lb_target_group" "abdelatif-tg-HTTPS" {
+  name        = "Abdelatif-TG-HTTPS"
+  target_type = "instance"
+  port        = 32001
+  protocol    = "HTTPS"
+  vpc_id      = var.vpc_id
+
+  health_check {
+    path                = "/healthz"
+    protocol            = "HTTPS"
+    matcher             = "200"
+    interval            = 15
+    timeout             = 5
+    healthy_threshold   = 2
+    unhealthy_threshold = 3
+  }
+
+  tags = {
+    Name     = "Abdelatif-TG-HTTPS"
+    owner    = local.tags.owner
+    ephemere = local.tags.ephemere
+    entity   = local.tags.entity
+  }
+}
+
 resource "aws_autoscaling_attachment" "asg_attachment1" {
   autoscaling_group_name = aws_eks_node_group.private-nodes.resources[0].autoscaling_groups[0].name
   lb_target_group_arn    = aws_lb_target_group.abdelatif-tg-HTTP.arn
+
+  depends_on = [
+    aws_eks_node_group.private-nodes
+  ]
+}
+
+resource "aws_autoscaling_attachment" "asg_attachment2" {
+  autoscaling_group_name = aws_eks_node_group.private-nodes.resources[0].autoscaling_groups[0].name
+  lb_target_group_arn    = aws_lb_target_group.abdelatif-tg-HTTPS.arn
 
   depends_on = [
     aws_eks_node_group.private-nodes
@@ -61,6 +97,25 @@ resource "aws_lb_listener" "abdelatif-lb-listener-http" {
 
   tags = {
     Name     = "abdelatif-lb-listener-http"
+    owner    = local.tags.owner
+    ephemere = local.tags.ephemere
+    entity   = local.tags.entity
+  }
+}
+
+resource "aws_lb_listener" "abdelatif-lb-listener-https" {
+  load_balancer_arn = aws_lb.abdelatif-alb.arn
+  port              = "443"
+  protocol          = "HTTPS"
+  certificate_arn   = "arn:aws:acm:eu-west-1:019050461780:certificate/bf7193d7-a525-4c87-9bc0-5d104bbae6a6"
+
+  default_action {
+    type             = "forward"
+    target_group_arn = aws_lb_target_group.abdelatif-tg-HTTPS.arn
+  }
+
+  tags = {
+    Name     = "abdelatif-lb-listener-https"
     owner    = local.tags.owner
     ephemere = local.tags.ephemere
     entity   = local.tags.entity
