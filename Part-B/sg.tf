ed4efa0e214b5a45e809b1bc21d680fc41418b25
Abdelatif AIT BARA <aitbaraabdelatif@outlook.com>
2023-11-16 10:08:25 +0100

    Fix ALB SG 3

--- Part-B/sg.tf
+++ Part-B/sg.tf
@@ -34,19 +34,6 @@ resource "aws_security_group" "alb-sg" {
 
 # Create a SG for EKS:
 
-resource "aws_security_group" "eks_cluster" {
-  name        = "Abdelatif-EKS-SG"
-  description = "Allow pods to communicate with each other"
-  vpc_id      = var.vpc_id
-
-  egress {
-    from_port   = 0
-    to_port     = 0
-    protocol    = "-1"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-}
-
 
 resource "aws_security_group_rule" "allow_alb_to_eks" {
   type              = "ingress"
@@ -54,31 +41,6 @@ resource "aws_security_group_rule" "allow_alb_to_eks" {
   to_port           = 32001
   protocol          = "-1"
   source_security_group_id = aws_security_group.alb-sg.id
-  security_group_id = aws_security_group.eks_cluster.id
-}
-
-
-# Create node group security group
-resource "aws_security_group" "node_group_sg" {
-  name        = "Abdelatif-Node_Group_SG"
-  description = "Security group for EKS node group"
-  vpc_id      = var.vpc_id
-  egress {
-    from_port   = 0
-    to_port     = 0
-    protocol    = "-1"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-
-}
-
-
-resource "aws_security_group_rule" "cluster_to_node_ingress" {
-  type              = "ingress"
-  from_port         = 0
-  to_port           = 0
-  protocol          = "-1"
-  source_security_group_id = aws_security_group.eks_cluster.id
-  security_group_id = aws_security_group.node_group_sg.id
+  security_group_id     = [aws_eks_cluster.K8sCluster.vpc_config[0].cluster_security_group_id]
 }
 
