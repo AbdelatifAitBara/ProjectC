965e94c191017e0866821d4e6c17cfb1f2cdb359
Abdelatif AIT BARA <aitbaraabdelatif@outlook.com>
2023-11-16 09:13:04 +0100

    Fix ALB SG

--- Part-B/sg.tf
+++ Part-B/sg.tf
@@ -10,12 +10,14 @@ resource "aws_security_group" "alb-sg" {
     protocol    = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
   }
-  ingress = {
+
+  ingress {
     from_port   = 443
     to_port     = 443
     protocol    = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
   }
+
   egress {
     from_port   = 0
     to_port     = 0
