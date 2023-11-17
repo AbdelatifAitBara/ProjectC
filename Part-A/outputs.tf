1dbc667d1691f697d8a2d2986389326717fb8b14
Abdelatif AIT BARA <aitbaraabdelatif@outlook.com>
2023-11-17 17:43:19 +0100

    Try to FIX TF Files

--- Part-A/outputs.tf
+++ Part-A/outputs.tf
@@ -10,6 +10,11 @@ output "ec2_ansible_ip" {
   value = aws_instance.ec2_ansible.public_ip
 }
 
+# Build Machine
+
+output "ec2_build_ip" {
+  value = aws_instance.ec2-bm.public_ip
+}
 
 # Vault EC2 OUTPUTS
 
