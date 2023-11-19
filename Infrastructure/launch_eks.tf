f78ff2d23aa63fc60fd43d513d10668abf88f23e
Abdelatif AIT BARA <aitbaraabdelatif@outlook.com>
2023-11-19 11:16:51 +0100

    Try to automate EKS Configuration

--- Infrastructure/launch_eks.tf
+++ Infrastructure/launch_eks.tf
@@ -1,5 +1,5 @@
 resource "null_resource" "run_command" {
     provisioner "local-exec" {
-        command = "sleep 240s && aws eks --region eu-west-1 update-kubeconfig --name Abdelatif-K8sCluster && kubectl create namespace argocd && kubectl apply -n argocd -f https://raw.githubusercontent.com/argoproj/argo-cd/stable/manifests/install.yaml" 
+        command = "sleep 600s && aws eks --region eu-west-1 update-kubeconfig --name Abdelatif-K8sCluster && kubectl create namespace argocd && kubectl apply -n argocd -f https://raw.githubusercontent.com/argoproj/argo-cd/stable/manifests/install.yaml" 
     }
 }
