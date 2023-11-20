e4a227bc3df2bcf4921a2ed5ad958dc74042983f
Abdelatif AIT BARA <aitbaraabdelatif@outlook.com>
2023-11-20 19:26:07 +0100

    Added ECR Microservices Repo

--- Infrastructure/ecr.tf
+++ Infrastructure/ecr.tf
@@ -1,7 +1,23 @@
-resource "aws_ecr_repository" "my-ecr-repo" {
-    name                 = "Abdelatif-ECR"
+resource "aws_ecr_repository" "customer-ecr" {
+    name                 = "abdelatif-ecr-customer"
     image_tag_mutability = "MUTABLE"
 
+    image_scanning_configuration {
+        scan_on_push = true
+    }
+}
+
+resource "aws_ecr_repository" "product-ecr" {
+    name                 = "abdelatif-ecr-product"
+    image_tag_mutability = "MUTABLE"
+    image_scanning_configuration {
+        scan_on_push = true
+    }
+}
+
+resource "aws_ecr_repository" "order-ecr" {
+    name                 = "abdelatif-ecr-order"
+    image_tag_mutability = "MUTABLE"
     image_scanning_configuration {
         scan_on_push = true
     }
