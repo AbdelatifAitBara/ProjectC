464719691a233b449aae5b7331b3500ef47de383
Abdelatif AIT BARA <aitbaraabdelatif@outlook.com>
2023-11-20 19:49:02 +0100

    Remove Scan From ECR

--- Infrastructure/ecr.tf
+++ Infrastructure/ecr.tf
@@ -1,24 +1,14 @@
 resource "aws_ecr_repository" "customer-ecr" {
     name                 = "abdelatif-ecr-customer"
     image_tag_mutability = "MUTABLE"
-
-    image_scanning_configuration {
-        scan_on_push = true
-    }
 }
 
 resource "aws_ecr_repository" "product-ecr" {
     name                 = "abdelatif-ecr-product"
     image_tag_mutability = "MUTABLE"
-    image_scanning_configuration {
-        scan_on_push = true
-    }
 }
 
 resource "aws_ecr_repository" "order-ecr" {
     name                 = "abdelatif-ecr-order"
     image_tag_mutability = "MUTABLE"
-    image_scanning_configuration {
-        scan_on_push = true
-    }
 }
