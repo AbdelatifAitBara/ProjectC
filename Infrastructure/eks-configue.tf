9cb9a4fa3188c7a07b2eeb47e4e8a066f01ea8e6
Abdelatif AIT BARA <aitbaraabdelatif@outlook.com>
2023-11-21 17:59:48 +0100

    Test The Whole INFRA

--- Infrastructure/eks-configue.tf
+++ Infrastructure/eks-configue.tf
@@ -3,7 +3,5 @@ resource "null_resource" "run_command" {
         command = "aws eks --region eu-west-1 update-kubeconfig --name Abdelatif-K8sCluster && kubectl create namespace argocd && kubectl apply -n argocd -f https://raw.githubusercontent.com/argoproj/argo-cd/stable/manifests/install.yaml && kubectl apply -f https://github.com/cert-manager/cert-manager/releases/download/v1.13.2/cert-manager.yaml" 
     }
 
-    triggers = {
-        cluster_ready = local.tags.eks_cluster_endpoint
-    }
+    depends_on = [ aws_eks_cluster.K8sCluster ]
 }
