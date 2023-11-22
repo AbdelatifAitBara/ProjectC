d810839b2c5e7ff0e4261cac33ea123a7e6d2ffe
Abdelatif AIT BARA <aitbaraabdelatif@outlook.com>
2023-11-22 20:52:45 +0100

    K6 Scan Fix 7

--- LoadingPerformanceTests/microservices/product/add-product-test.js
+++ LoadingPerformanceTests/microservices/product/add-product-test.js
@@ -1,17 +1,11 @@
+import { check } from 'k6';
 import http from 'k6/http';
 
-
-export default function() {
-
-
+export default function () {
   http.setResponseCallback(http.expectedStatuses(405));
   const res = http.get('https://projetc.abdelatif-aitbara.link/product/add_product');
-
-  if(res.body.includes('Method Not Allowed')) {
-    check(res, {
-      'verify homepage text': (r) => {
-        return true;  
-      }
-    });
-  }
+  check(res, {
+    'verify homepage text': (r) =>
+      r.body.includes('Method Not Allowed'),
+  });
 }
