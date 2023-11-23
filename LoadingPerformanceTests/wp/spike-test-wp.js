4e716f6163c85f23fbd633ec441398fd697f0425
Abdelatif AIT BARA <aitbaraabdelatif@outlook.com>
2023-11-23 17:57:34 +0100

    BetaTest Of K6

--- LoadingPerformanceTests/wp/spike-test-wp.js
+++ LoadingPerformanceTests/wp/spike-test-wp.js
@@ -4,7 +4,7 @@ import {sleep} from 'k6';
 export const options = {
     // Key configurations for spike in this section
     stages: [
-        { duration: '2m', target: 100 }, // fast ramp-up to a high point
+        { duration: '2m', target: 50 }, // fast ramp-up to a high point
         // No plateau
         { duration: '1m', target: 0 }, // quick ramp-down to 0 users
     ],
