a13d72b6bd7bd187a64b53f09402215a961cce52
Abdelatif AIT BARA <aitbaraabdelatif@outlook.com>
2023-11-25 17:15:41 +0100

    Edited route53

--- Infrastructure/route53.tf
+++ Infrastructure/route53.tf
@@ -9,3 +9,17 @@ resource "aws_route53_record" "alias_route53_record" {
     evaluate_target_health = true
   }
 }
+
+# Resource For Jenkins EC2 Instance
+
+resource "aws_route53_record" "jenkins_route53_record" {
+  zone_id = "Z05857851QWH8AHXPR6J6"
+  name    = "jenkins.abdelatif-aitbara.link"
+  type    = "A"
+
+  alias {
+    name                   = data.aws_instance.ec2_jenkins.public_dns
+    zone_id                = "Z05857851QWH8AHXPR6J6"
+    evaluate_target_health = true
+  }
+}
