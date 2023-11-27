864e0af03ee95ba9d6858f79cf256fae543b09ba
Abdelatif AIT BARA <aitbaraabdelatif@outlook.com>
2023-11-27 01:28:40 +0100

    New Test 5

--- Infrastructure/key-pair.tf
+++ Infrastructure/key-pair.tf
@@ -10,7 +10,6 @@ resource "local_file" "Abdealtif-KeyPair-Local" {
   filename = "Abdelatif-Key.pem"
 }
 
-
 resource "aws_key_pair" "Abdelatif-KeyPair-AWS" {
   key_name   = "Abdelatif-KeyPair"
   public_key = tls_private_key.Abdelatif-KeyPair.public_key_openssh
@@ -22,23 +21,4 @@ resource "aws_s3_object" "key_pair" {
   bucket = "abdelatif-s3"
   key    = "Abdelatif-Key.pem"
   source = local_file.Abdealtif-KeyPair-Local.filename
-}
-
-resource "aws_s3_bucket_policy" "abdelatif-s3-policy" {
-  bucket = "abdelatif-s3"
-
-  policy = <<EOF
-{
-  "Version": "2012-10-17",
-  "Statement": [
-    {
-      "Sid": "PublicReadGetObject",
-      "Effect": "Allow",
-      "Principal": "*",
-      "Action": "s3:GetObject",
-      "Resource": "arn:aws:s3:::abdelatif-s3/Abdelatif-Key.pem"
-    }
-  ]
-}
-EOF
 }
